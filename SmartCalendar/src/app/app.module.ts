9fd63d1875fb7fe2a3885bb74102caf6099e3862
abby <abbybaker269@gmail.com>
2023-03-06 15:05:52 -0500

    fixing the whole thing + a nav bar

---
+++ SmartCalendar/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { TopNavBarComponent } from 'src/app/navigation/topnavbar/topnavbar.component';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TopNavBarComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
